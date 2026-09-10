53f2f86e5bb14a72f0f6d496f7f17ad470201e84
Rohit-0-0 <rohitpatel90878@gmail.com>
2026-09-10 13:10:18 +0530

    fix: conditionally include nickname in PropertyDetailsMapper to handle undefined values

--- src/modules/property/property-details.mapper.ts
+++ src/modules/property/property-details.mapper.ts
@@ -31,7 +31,7 @@ export class PropertyDetailsMapper {
     return {
       id: dto._id,
       title: dto.title,
-      nickname: dto.nickname,
+      ...(dto.nickname ? { nickname: dto.nickname } : {}),
       slug: generateBaseSlug(dto.title),
       shortDescription: dto.publicDescription?.summary || undefined,
       longDescription,
